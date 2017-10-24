b8f537dc341130412d20a68605f82faadc5d4429
sandesh.kd@robosoftin.com <sandesh.kd@robosoftin.com>
2017-10-24 12:39:18 +0530

    reordered

--- src/directives/parallax/parallax.ts
+++ src/directives/parallax/parallax.ts
@@ -16,14 +16,14 @@ export class ParallaxDirective {
   translateAmount: any;
   scaleAmount: any;
 
-  constructor( private el: ElementRef, private renderer: Renderer) {
+  constructor(private el: ElementRef, private renderer: Renderer) {
   }
 
   ngOnInit() {
     let content = this.el.nativeElement.getElementsByClassName('scroll-content')[0];
     this.header = content.getElementsByClassName('details-img')[0];
     this.main = content.getElementsByClassName('details-main')[0];
-  
+
     this.headerHeight = this.header.clientHeight;
 
     this.renderer.setElementStyle(this.header, 'webTransformOrigin', 'center bottom');
@@ -35,19 +35,19 @@ export class ParallaxDirective {
     console.log(event);
     event.domWrite(() => {
       this.update(event);
-    }); 
+    });
   }
 
   update(event) {
-    if(event.scrollTop >= 0) {
-      this.translateAmount = event.scrollTop/3;
+    if (event.scrollTop >= 0) {
+      this.translateAmount = event.scrollTop / 3;
       this.scaleAmount = 1;
     } else {
       this.translateAmount = 0;
       this.scaleAmount = -event.scrollTop / this.headerHeight + 1;
-  }
-    this.renderer.setElementStyle(this.header, 'webkitTransform', 'translate3d(0,'+this.translateAmount+'px,0) scale(' + this.scaleAmount + ', ' + this.scaleAmount + ')');
-    
+    }
+    this.renderer.setElementStyle(this.header, 'webkitTransform', 'translate3d(0,' + this.translateAmount + 'px,0) scale(' + this.scaleAmount + ', ' + this.scaleAmount + ')');
+
   }
 
 }
