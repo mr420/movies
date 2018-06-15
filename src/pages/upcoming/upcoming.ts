fa5cfd32058aefe41a047aa995adbd4703854c99
sandesh <newsandesh1@gmail.com>
2018-06-15 09:38:25 +0530

    console removed

--- src/pages/upcoming/upcoming.ts
+++ src/pages/upcoming/upcoming.ts
@@ -1,45 +1,39 @@
-import { MoviedbProvider } from './../../providers/moviedb/moviedb';
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import { MoviedbProvider } from "./../../providers/moviedb/moviedb";
+import { Component } from "@angular/core";
+import { IonicPage, NavController, NavParams } from "ionic-angular";
 
 @IonicPage()
 @Component({
-  selector: 'page-upcoming',
-  templateUrl: 'upcoming.html',
+  selector: "page-upcoming",
+  templateUrl: "upcoming.html"
 })
 export class UpcomingPage {
-
   movies: any;
   errorMessage: string;
-  imageBaseUrl = 'http://image.tmdb.org/t/p/w185//';
+  imageBaseUrl = "http://image.tmdb.org/t/p/w185//";
 
-  constructor(public navCtrl: NavController, public navParams: NavParams,
-    public moviedb: MoviedbProvider) {
-  }
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public moviedb: MoviedbProvider
+  ) {}
 
   ionViewDidLoad() {
     this.getUpcomingMovies();
-    console.log('ionViewDidLoad UpcomingPage');
+    console.log("ionViewDidLoad UpcomingPage");
   }
 
   getUpcomingMovies() {
-    this.moviedb.getUpcomingMovies()
-      .subscribe(
-      response => {
+    this.moviedb.getUpcomingMovies().subscribe(response => {
       this.movies = response.results;
-        console.log(this.movies)
-      },
-      error => this.errorMessage = <any>error
-      );
+    }, error => (this.errorMessage = <any>error));
   }
 
   showDetail(movie) {
-    this.navCtrl.push('DetailsPage', { details: movie });
+    this.navCtrl.push("DetailsPage", { details: movie });
   }
 
   gotoSettings() {
-    this.navCtrl.push('SettingsPage');
+    this.navCtrl.push("SettingsPage");
   }
-
 }
